903c5208c88f0a13641bc45df9069ec0f30b94e0
OI-Biostat <38590928+dave-harrington@users.noreply.github.com>
2023-09-08 11:08:05 -0400

    corrections to exercises

--- ch_inference_foundations_oi_biostat/figures/pValueSleep/pValueSleep.R
+++ ch_inference_foundations_oi_biostat/figures/pValueSleep/pValueSleep.R
@@ -4,13 +4,13 @@ data(COL)
 myPDF('pValueSleep.pdf', 6, 2.4,
       mar = c(2, 0, 0.5, 0),
       mgp = c(3, 0.65, 0))
-normTail(L = -0.864,
+normTail(L = -0.836,
          col = COL[1],
          axes  =  FALSE,
          lwd  =  2)
-at <- c(-5, 0, -0.864, 5)
+at <- c(-5, 0, -0.836, 5)
 labels <- expression(0, ''*mu*' = 0  ',
-                     't = -0.864', 0)
+                     't = -0.836', 0)
 axis(1, at, labels, cex.axis = 1)
 yMax <- 0.4
 
