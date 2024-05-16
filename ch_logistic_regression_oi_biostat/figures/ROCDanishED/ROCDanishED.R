395aa4f144d53c59eb4c561ce77678da6a40f1f0
OI-Biostat <38590928+dave-harrington@users.noreply.github.com>
2024-05-16 11:36:12 -0400

    Edits to logistic case study

--- ch_logistic_regression_oi_biostat/figures/ROCDanishED/ROCDanishED.R
+++ ch_logistic_regression_oi_biostat/figures/ROCDanishED/ROCDanishED.R
@@ -19,6 +19,20 @@ m = glm(mort30 ~ triage + age + sex + age*sex, data = d.primary,
 pred.m <- prediction(m$fitted.values, d.primary$mort30)
 perf.m <- performance(pred.m, "tpr", "fpr")
 
+# model with dept only
+
+m.dept = glm(mort30 ~ triage, data = d.primary,
+             family = "binomial")
+fitted.m.dept = predict(m.dept, newdata = d.valid,
+                        type = 'response')
+
+pred.m.dept = prediction(fitted.m.dept,
+                         d.valid$mort30)
+perf.m.dept = performance(pred.m.dept, "tpr", "fpr")
+
+fpr.dept = round(as.vector(perf.m.dept@x.values[[1]]), digits = 3)
+tpr.dept = round(as.vector(perf.m.dept@y.values[[1]]), digits = 3)
+
 # ns1 <- 4
 myPDF("ROCDanishED.pdf", 7.7, 4.4,
       mar = c(3.9, 7, 0.5, 0.2),
@@ -31,51 +45,8 @@ plot(perf.m,
 
 abline(a = 0, b = 1)
 
-# interaction model, test data
-
-fitted.values = predict(m, newdata = d.valid, 
-                                type = 'response')
-
-pred.m = prediction(fitted.values, 
-                              d.valid$mort30)
-perf.m = performance(pred.m, "tpr", "fpr")
-
-fpr = round(as.vector(perf.m@x.values[[1]]), digits = 3)
-tpr = round(as.vector(perf.m@y.values[[1]]), digits = 3)
-
-lines(fpr, tpr, 
-      lty = 2, col = "blue")
-
-# DEPT classification, primary data
-
-m = glm(mort30 ~ triage, data = d.primary,
-                family = "binomial")
-
-
-pred.m <- prediction(m$fitted.values, d.primary$mort30)
-perf.m <- performance(pred.m, "tpr", "fpr")
-
-
-fpr = round(as.vector(perf.m@x.values[[1]]), digits = 3)
-tpr = round(as.vector(perf.m@y.values[[1]]), digits = 3)
-
-lines(fpr, tpr, 
+lines(fpr.dept, tpr.dept,
       lty = 1, col = "darkgreen")
 
-# DEPT classification, test data
-
-m = glm(mort30 ~ triage, data = d.valid,
-        family = "binomial")
-
-pred.m <- prediction(m$fitted.values, d.valid$mort30)
-perf.m <- performance(pred.m, "tpr", "fpr")
-
-
-fpr = round(as.vector(perf.m@x.values[[1]]), digits = 3)
-tpr = round(as.vector(perf.m@y.values[[1]]), digits = 3)
-
-lines(fpr, tpr, 
-      lty = 2, col = "darkgreen")
-
-
 dev.off()
+
